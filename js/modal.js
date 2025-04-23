25c41220e0fd557b974d12ade1cf9c0809e6b92d
MaximRahimkhanov <zloyigrog@gmail.com>
2025-04-23 14:26:44 +0300

    add java modal

---
+++ js/modal.js
@@ -0,0 +1,9 @@
+(() => {
+  const refs = {
+    // Додати атрибут data-modal-open на кнопку відкриття
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    // Додати атрибут data-modal-close на кнопку закриття
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    // Додати атрибут data-modal на бекдроп модалки
+    modal: document.querySelector("[data-mgiodal]"),
+  };
